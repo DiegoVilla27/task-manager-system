df6b0ffa479f9b5f93beed3927102771f350c207
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2026-08-16 13:45:24 +0200

    test(users): add unit tests for user service (#16)

---
+++ task-manager-client/src/features/users/service/index.test.ts
@@ -0,0 +1,47 @@
+import { httpService } from '@core/http';
+import type { UserMeResponse } from '../interfaces/response';
+import { getMeSvc } from '.';
+
+vi.mock('@core/http', () => ({
+  httpService: {
+    get: vi.fn(),
+  },
+}));
+
+describe('Users service', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    vi.clearAllMocks();
+  });
+
+  it('should get current user', async () => {
+    // Arrange
+    const user: UserMeResponse = {
+      id: '1',
+      name: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+    };
+    vi.mocked(httpService.get).mockReturnValue(Promise.resolve(user));
+    // Act
+    const result = await getMeSvc();
+    // Assert
+    expect(result).toEqual(user);
+    expect(httpService.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should save user in local storage', async () => {
+    // Arrange
+    const user: UserMeResponse = {
+      id: '1',
+      name: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+    };
+    vi.mocked(httpService.get).mockReturnValue(Promise.resolve(user));
+    // Act
+    await getMeSvc();
+    // Assert
+    expect(localStorage.getItem('ME')).toEqual(JSON.stringify(user));
+  });
+});
