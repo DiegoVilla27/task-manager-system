80b0747cbd42b7043fb216b63c338fb0a82e827d
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2026-08-16 19:00:47 +0200

    test(auth): add unit tests for register page and hook (#19)

---
+++ task-manager-client/src/features/auth/pages/register/hooks/index.test.ts
@@ -0,0 +1,91 @@
+import { act, renderHook } from '@testing-library/react';
+import { useNavigate } from 'react-router-dom';
+import useRegisterPage from '.';
+import { registerSvc } from '@features/auth/services';
+import type { AuthResponse } from '@features/auth/interfaces/response';
+import type { AuthRegisterRequest } from '@features/auth/interfaces/request';
+
+vi.mock('@features/auth/services', () => ({
+  registerSvc: vi.fn(),
+}));
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: vi.fn(),
+}));
+
+describe('Auth: useRegisterPage', () => {
+  const mockedNavigate = vi.fn();
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.mocked(useNavigate).mockReturnValue(mockedNavigate);
+  });
+
+  it('should render with default values', () => {
+    // Act
+    const { result } = renderHook(() => useRegisterPage());
+    // Assert
+    expect(result.current.errors).toEqual({});
+    expect(result.current.register).toBeTypeOf('function');
+    expect(result.current.submit).toBeTypeOf('function');
+  });
+
+  it('should submit form with value data', async () => {
+    // Arrange
+    const payload: AuthRegisterRequest = {
+      name: 'Diego',
+      lastname: 'Villa',
+      email: 'dv@gmail.com',
+      password: 'password_123',
+      confirmPassword: 'password_123',
+    };
+    const authRes: AuthResponse = {
+      access_token: 'token_123',
+      refresh_token: 'refresh_123',
+      expires_in: 3600,
+    };
+    vi.mocked(registerSvc).mockReturnValue(Promise.resolve(authRes));
+    // Act
+    const { result } = renderHook(() => useRegisterPage());
+    act(() => {
+      result.current.setValue('name', payload.name);
+      result.current.setValue('lastname', payload.lastname);
+      result.current.setValue('email', payload.email);
+      result.current.setValue('password', payload.password);
+      result.current.setValue('confirmPassword', payload.confirmPassword);
+    });
+    await act(async () => {
+      await result.current.submit();
+    });
+    // Assert
+    expect(registerSvc).toHaveBeenCalledTimes(1);
+    expect(registerSvc).toHaveBeenCalledWith(payload);
+    expect(mockedNavigate).toHaveBeenCalledWith('/');
+  });
+
+  it('should return error if payload is invalid', async () => {
+    // Arrange
+    const payload: AuthRegisterRequest = {
+      name: '',
+      lastname: 'Villa',
+      email: 'dv@gmail.com',
+      password: 'password_123',
+      confirmPassword: 'password_123',
+    };
+    // Act
+    const { result } = renderHook(() => useRegisterPage());
+    act(() => {
+      result.current.setValue('name', payload.name);
+      result.current.setValue('lastname', payload.lastname);
+      result.current.setValue('email', payload.email);
+      result.current.setValue('password', payload.password);
+      result.current.setValue('confirmPassword', payload.confirmPassword);
+    });
+    await act(async () => {
+      await result.current.submit();
+    });
+    // Assert
+    expect(registerSvc).not.toHaveBeenCalled();
+    expect(result.current.errors.name?.message).toEqual('Name must be at least 3 characters long');
+  });
+});
