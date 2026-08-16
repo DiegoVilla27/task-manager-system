d294b174e841fed0c98bc33630e661048464113b
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2026-08-16 13:39:47 +0200

    test(auth): add unit tests for auth services and layout (#15)

---
+++ task-manager-client/src/features/auth/services/index.test.ts
@@ -0,0 +1,79 @@
+import { httpService } from '@core/http';
+import type { AuthResponse } from '../interfaces/response';
+import { loginSvc, logoutSvc, registerSvc } from '.';
+import type { AuthLoginRequest, AuthRegisterRequest } from '../interfaces/request';
+import type { UserMeResponse } from '@features/users/interfaces/response';
+
+vi.mock('@core/http', () => ({
+  httpService: {
+    get: vi.fn(),
+    post: vi.fn(),
+  },
+}));
+
+const userRes: UserMeResponse = {
+  id: '1',
+  name: 'Diego',
+  lastname: 'Villa',
+  email: 'dv@gmail.com',
+};
+const authRes: AuthResponse = {
+  access_token: 'token_123',
+  refresh_token: 'refresh_123',
+  expires_in: 3600,
+};
+
+describe('Auth: Services', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    vi.clearAllMocks();
+  });
+
+  it('should login to user successfully', async () => {
+    // Arrange
+    const payload: AuthLoginRequest = {
+      email: 'dv@gmail.com',
+      password: '12345678',
+    };
+    vi.mocked(httpService.post).mockReturnValue(Promise.resolve(authRes));
+    vi.mocked(httpService.get).mockReturnValue(Promise.resolve(userRes));
+    // Act
+    const res = await loginSvc(payload);
+    // Assert
+    expect(res).toEqual(authRes);
+    expect(httpService.post).toHaveBeenCalledTimes(1);
+    expect(httpService.get).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem('TOKEN')).toEqual(authRes.access_token);
+    expect(localStorage.getItem('REFRESH')).toEqual(authRes.refresh_token);
+  });
+
+  it('should register to user successfully', async () => {
+    // Arrange
+    const payload: AuthRegisterRequest = {
+      name: 'Diego',
+      lastname: 'Villa',
+      email: 'dv@gmail.com',
+      password: '12345678',
+      confirmPassword: '12345678',
+    };
+    vi.mocked(httpService.post).mockReturnValue(Promise.resolve(authRes));
+    vi.mocked(httpService.get).mockReturnValue(Promise.resolve(userRes));
+    // Act
+    const res = await registerSvc(payload);
+    // Assert
+    expect(res).toEqual(authRes);
+    expect(httpService.post).toHaveBeenCalledTimes(1);
+    expect(httpService.get).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem('TOKEN')).toEqual(authRes.access_token);
+    expect(localStorage.getItem('REFRESH')).toEqual(authRes.refresh_token);
+  });
+
+  it('should logout successfully', () => {
+    // Act
+    logoutSvc();
+    // Assert
+    expect(localStorage.getItem('TOKEN')).toBeNull();
+    expect(localStorage.getItem('REFESH')).toBeNull();
+    expect(localStorage.getItem('ME')).toBeNull();
+  });
+});
